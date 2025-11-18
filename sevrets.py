ba5b1416750cd7764a5e81db6127fa128443b43f
Paul Peck <peckx257@gmail.com>
2025-11-18 09:36:26 -0500

    Create sevrets.py
    This is where your network name, password, and timezone live

---
+++ sevrets.py
@@ -0,0 +1,7 @@
+# secrets.py
+secrets = {
+    "ssid": "INSERT_NETWORK_NAME_HERE",
+    "password": "INSERT_NETWORK_PASSWORD_HERE",
+    "timezone": "Region/City",  # your TZ IANA timezone format
+
+}
