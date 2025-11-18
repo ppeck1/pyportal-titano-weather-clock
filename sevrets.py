db37e9c02214d9a5619e6fee7039ed3891956e6b
Paul Peck <peckx257@gmail.com>
2025-11-18 09:41:08 -0500

    Delete sevrets.py
    Naming error - replaced

--- sevrets.py
+++
@@ -1,7 +0,0 @@
-# secrets.py
-secrets = {
-    "ssid": "INSERT_NETWORK_NAME_HERE",
-    "password": "INSERT_NETWORK_PASSWORD_HERE",
-    "timezone": "Region/City",  # your TZ IANA timezone format
-
-}
